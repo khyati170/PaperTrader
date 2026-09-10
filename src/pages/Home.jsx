00c5d5767434afab51c83a5b5320fe830ca1e8a8
khyati <khyati30bnl@gmail.com>
2026-09-11 00:07:59 +0530

    modified home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
-import Hero from "../components/Hero";
+import Hero from "../components/hero";
 import FeatureSection from "../components/FeatureSection";
 import { features } from "../data/features";
+import HowItworks from "../components/HowItworks";
 
 function Home() {
   return (
     <>
       <Hero />
+      <HowItworks />
 
       {features.map((feature, index) => (
         <FeatureSection
           key={feature.id}
           variant={index % 2 === 0 ? "one" : "two"}
-          icon = {feature.icon}
+          icon={feature.icon}
           title={feature.title}
           text={feature.text}
         />
